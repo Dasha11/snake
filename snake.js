e7e0dede13cd687862aee76ed44e2386016dce13
Dasha11 <dashajq@gmail.com>
2016-05-24 14:17:09 +0600

    Add files via upload

--- snake.js
+++ snake.js
@@ -5,79 +5,79 @@
  body:["3_10","2_10","1_10"]
  };
  var food=""; 
- function myinit(){
-    dir=1;
-    snake=["3_10","2_10","1_10"];
-    food="";
-    $('#div_main').html("");
-    for (var r=0;r<20;r++){
-      for (var c=0;c<20;c++){
-	$('#div_main').append('<div class=mycell id=c_'+r+'_'+c+'></div>');
-      }
-    }
-      $('#c_1_10').addClass('snake');
-      $('#c_2_10').addClass('snake');
-      $('#c_3_10').addClass('snake');
-      generatefood();
- }
- myinit();
- function generatefood(){
-    var r1 = Math.floor(Math.random() * 19);
-    var c1 = Math.floor(Math.random() * 19);
-    $('#c_'+r1+'_'+c1).addClass('eat');
-    food=''+r1+'_'+c1;
-	
- } 
- function gameupdate(){
-  var tail=snake.pop(); 
-  $('#c_'+tail).removeClass('snake');
-  var hh=snake[0];
-  var rc=hh.split("_");
-  var r=parseInt(rc[0]);
-  var c=parseInt(rc[1]);
-  switch(dir){
-    case 1: r=r+1; break; // Bottom
-    case 2: c=c-1; break; // Left
-    case 3: r=r-1; break; // Top
-    case 4: c=c+1; break;  // Right
-  }  
-  var snakeLength=""+r+"_"+c;
-  if (snakeLength==food){
-      snake.push(tail);
-      $('#c_'+tail).addClass('snake');
-      $('#c_'+food).removeClass('eat');
-      generatefood();
-	var score = snake.length-3;
-	document.getElementById('score').innerHTML = 'Счёт: '+(score+1);
-  }
-  snake.unshift(snakeLength);
-  $('#c_'+snakeLength).hasClass('snake'); 
-  if (c<0 || r<0 || c>19 || r>19 ||  $('#c_'+snakeLength).hasClass('snake') ){
-    alert('Game Over! Счёт ' + (snake.length-3) + '. Press Reload for new game');    
-    return;
-  }  
-  $('#c_'+snakeLength).addClass('snake');       
-  setTimeout(function(){gameupdate()}, speed);
- } 
- $(document).keydown(function(e){
-    if (e.keyCode == 37) { 
-       dir=2;
-    }else if (e.keyCode == 38) { 
-       dir=3;
-    }else if (e.keyCode == 39) { 
-       dir=4;
-    }else if (e.keyCode == 40) { 
-       dir=1;
-    }
-});
-function Start () {
+	 function myinit(){
+		dir=1;
+		snake=["3_10","2_10","1_10"];
+		food="";
+		$('#div_main').html("");
+		for (var r=0;r<20;r++){
+		  for (var c=0;c<20;c++){
+		$('#div_main').append('<div class=mycell id=c_'+r+'_'+c+'></div>');
+		  }
+		}
+		  $('#c_1_10').addClass('snake');
+		  $('#c_2_10').addClass('snake');
+		  $('#c_3_10').addClass('snake');
+		  generatefood();
+	 }
+	myinit();
+	 function generatefood(){
+		var r1 = Math.floor(Math.random() * 19);
+		var c1 = Math.floor(Math.random() * 19);
+		$('#c_'+r1+'_'+c1).addClass('eat');
+		food=''+r1+'_'+c1;
+		
+	 } 
+	 function gameupdate(){
+		  var tail=snake.pop(); 
+		  $('#c_'+tail).removeClass('snake');
+		  var hh=snake[0];
+		  var rc=hh.split("_");
+		  var r=parseInt(rc[0]);
+		  var c=parseInt(rc[1]);
+			  switch(dir){
+				case 1: r=r+1; break; // Bottom
+				case 2: c=c-1; break; // Left
+				case 3: r=r-1; break; // Top
+				case 4: c=c+1; break;  // Right
+			  }  
+		  var snakeLength=""+r+"_"+c;
+			  if (snakeLength==food){
+				  snake.push(tail);
+				  $('#c_'+tail).addClass('snake');
+				  $('#c_'+food).removeClass('eat');
+				  generatefood();
+				var score = snake.length-3;
+				document.getElementById('score').innerHTML = 'Счёт: '+(score+1);
+			  }
+	   snake.unshift(snakeLength);
+	   $('#c_'+snakeLength).hasClass('snake'); 
+			  if (c<0 || r<0 || c>19 || r>19 ||  $('#c_'+snakeLength).hasClass('snake') ){
+				alert('Game Over! Счёт ' + (snake.length-3) + '. Press Reload for new game');    
+				return;
+			  }  
+	   $('#c_'+snakeLength).addClass('snake');       
+	   setTimeout(function(){gameupdate()}, speed);
+	 } 
+	$(document).keydown(function(e){
+		if (e.keyCode == 37) { 
+		   dir=2;
+		}else if (e.keyCode == 38) { 
+		   dir=3;
+		}else if (e.keyCode == 39) { 
+		   dir=4;
+		}else if (e.keyCode == 40) { 
+		   dir=1;
+		}
+	});
+	function Start () {
 
-        setTimeout(function(){gameupdate()}, speed);
+			setTimeout(function(){gameupdate()}, speed);
 
-    }
+	}
 
-    function reload () {
+		function reload () {
 
-        window.location.reload();
+			window.location.reload();
 
-    }
+	}
